5684ceaf4ca423cda93383949e053d1fa4f26a2a
pedro <alexismoreauavila@gmail.com>
2025-10-11 18:15:33 +0200

    tokn hided

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -4,8 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   projectId: "nvbey0vw",
   dataset: "studio",
-  token:
-    "sk3ZwhH5ANe0gNSSjDZF0cWv1ouVUYcAPKY1dpBa0YYlklip9eY58hrAevcUxOgTj63l3DDPI6UCaDwEj8qa4tdhHDqBvFvnvLzHSwd15ANe4ZEcGMfoq52Ug0nFLMqQ8er4l4Ff7aAQFcCQ5Az7wVUTs7283d5pnLFVq1UQ41xZ1OKEC1kq",
+
   useCdn: true,
   apiVersion: "2024-01-01",
 });
