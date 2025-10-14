24c3c449ed59376270f2afe772d538c36d1432ed
pedro <alexismoreauavila@gmail.com>
2025-10-14 20:10:08 +0200

    text align jjustify

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   * {
-text-align: right;
+text-align: justify;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
